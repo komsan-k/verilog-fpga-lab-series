538c061783dc639577ced9681092dd9a0c4d679a
Komsan Kanjanasit <94804188+komsan-k@users.noreply.github.com>
2025-11-13 09:03:03 +0700

    Update new_tb_sequential_multiplier.v

--- Lab_12_Multiplier/new_tb_sequential_multiplier.v
+++ Lab_12_Multiplier/new_tb_sequential_multiplier.v
@@ -25,10 +25,10 @@ module seq_multiplier_tb;
         for (integer i = 1; i <= 10; i = i + 1) begin
             B = i[3:0];          // Set B = 1 to 10
             start = 1;           // Trigger multiplication
-            #10 start = 0;       // Deassert start
+            #1 start = 0;       // Deassert start
             wait (done);         // Wait until multiplication completes
             $display("A = %0d, B = %0d => Product = %0d", A, B, product);
-            #20;                 // Small delay before next iteration
+            #2;                 // Small delay before next iteration
         end
 
         $finish;
